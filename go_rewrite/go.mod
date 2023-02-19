78904f2b6d722db49f0c6d06cdb84c50103d13c3
Ollie Etherington <ollie@etherington.xyz>
2023-02-19 21:38:15 +0000

    Generate index and styles

--- go_rewrite/go.mod
+++ go_rewrite/go.mod
@@ -2,4 +2,8 @@ module github.com/oetherington/oetherington.github.io
 
 go 1.19
 
-require github.com/oetherington/smetana v0.1.0 // indirect
+require (
+	github.com/oetherington/smetana v0.1.0 // indirect
+	github.com/otiai10/copy v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- go_rewrite/main.go
+++ go_rewrite/main.go
@@ -2,18 +2,62 @@ package main
 
 import (
 	"fmt"
-	. "github.com/oetherington/smetana"
+	"log"
+	"os"
+
+	"github.com/oetherington/smetana"
+	"github.com/otiai10/copy"
 )
 
+const PUBLIC_DIR = "./public"
+const OUTPUT_DIR = "./docs"
+
+func writeString(value string, targetName string) error {
+	targetPath := fmt.Sprintf("%s/%s", OUTPUT_DIR, targetName)
+
+	file, err := os.Create(targetPath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(value)
+	return err
+}
+
+func generateHtml(node smetana.Node, targetName string) error {
+	html := smetana.RenderHtml(node)
+	return writeString(html, targetName)
+}
+
+func generateCss(stylesheet smetana.StyleSheet, targetName string) error {
+	html := smetana.RenderCss(stylesheet)
+	return writeString(html, targetName)
+}
+
 func main() {
+	err := os.RemoveAll(OUTPUT_DIR)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = copy.Copy(PUBLIC_DIR, OUTPUT_DIR)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	palette := createPalette()
+
 	styles := createStyles(palette)
+	err = generateCss(styles, "css/styles.css")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	page := Layout(palette, "", Index())
-
-	css := RenderCss(styles)
-	html := RenderHtml(page)
-
-	fmt.Println(css)
-	fmt.Println(html)
+	index := Layout(palette, "", Index())
+	err = generateHtml(index, "index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
